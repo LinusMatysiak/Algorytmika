cb972ca2cf63c1d1114f48f46ddb13e644faf9fb
Linus Matysiak (Uczeń) <lmatysiak367@technikumkreatywne.pl>
2023-10-11 10:51:51 +0200

    2.2.5

---
+++ Potęgowanie-szybkie/Potęgowanie-szybkie/Potęgowanie-szybkie.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int potegowanie(int a, int n);
+int main()
+{
+	int a;
+	int n;
+	cout << "Podaj podstawe";
+	cin >> a;
+	cout << "Podaj wykładnik";
+	cin >> n;
+
+	cout << a << " do potęgi " << n << " wynosi: " << potegowanie(a, n);
+}
+
+int potegowanie(int a, int n) {
+	int w = 1;
+	while (n > 0) {
+		if (n % 2 == 1) {
+			w = w * a;
+		}
+		a = a * a;
+		n = n / 2;
+	}
+	return w;
+}
