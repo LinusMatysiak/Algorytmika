e9f1d51ae5b1b154ab2b9df4a7c8dfa9408f5c48
Linus <94894358+LinusMatysiak@users.noreply.github.com>
2024-05-17 13:00:51 +0200

    2-9-02-wydawanie-reszty

---
+++ 2-9-02-wydawanie-reszty/reszta/reszta.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+void reszta(int kwota);
+int input(std::string wypisz);
+int main()
+{
+	reszta(input("podaj kwote"));
+}
+void reszta(int kwota) {
+	int nominaly[9]{ 500,200,100,50,20,10,5,2,1 };
+	int i = 0;
+	while (kwota > 0) {
+		if (kwota >= nominaly[i]) {
+			int ile = kwota / nominaly[i];
+			kwota -= (nominaly[i] * ile);
+			std::cout << nominaly[i] << " x " << ile << std::endl;
+		}
+		i++;
+	}
+	return;
+}
+int input(std::string wypisz) {
+	std::cout << wypisz + ": ";
+	int wynik;
+	std::cin >> wynik;
+	return wynik;
+}
